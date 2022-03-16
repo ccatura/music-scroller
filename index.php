cc830319d580e89ac70aed244e171333f8fbcf97
ccatura <charlie@charliecatura.com>
2022-03-16 10:04:38 -0400

    created pwd file

--- index.php
+++ index.php
@@ -10,36 +10,6 @@
     <title>Charlie's Music Scroller for Musicians</title>
 </head>
 <body>
-
-
-
-<?php
-$servername = "sql647.main-hosting.eu";
-$username = "u682819236_ccatura_music";
-$password = "";
-$dbname = "u682819236_music_scroller";
-
-$message=""; //will hold message to display when authenticated
-
-//checks if username and password match what is in the database
-if(count($_POST)>0) {
-	$conn = mysqli_connect($servername, $username, $password, $dbname);
-	$result = mysqli_query($conn,"SELECT * FROM users WHERE userName='" . $_POST["userName"] . "' and password = '". $_POST["password"]."'");
-	$count  = mysqli_num_rows($result);
-	if($count==0) {
-		$message = "Invalid Username or Password!";
-	} else {
-		$message = "<span style='color: #0bb335;'>You are successfully authenticated!</span><br>";
-	}
-}
-
-if($message!="") {
-    echo "<br>" . $message;
-}
-?>
-
-
-
     
 <div class="wrapper">
     <div class="file-ops">
@@ -55,7 +25,7 @@ if($message!="") {
         <div class="vert-space"></div>
         <h5>Load Song</h5>
         <ul class="file-list">
-            <h6>Sort By: <a href="./index.php?sort=artist">Artist</a> | <a href="./index.php?sort=title">Title</a> | <a href="./index.php?sort=setlist">Setlist</a></h6>
+            <h6>Sort By: <a href="./index.php?sort=artist">Artist</a> | <a href="./index.php?sort=title">Title</a></h6>
 
         <?php
             include("./php_scripts/file_list.php");
--- php_scripts/file_list.php
+++ php_scripts/file_list.php
@@ -1,4 +1,27 @@
 <?php
+$servername = "sql647.main-hosting.eu";
+$username = "u682819236_ccatura_music";
+
+include("pword.php");
+
+$dbname = "u682819236_music_scroller";
+
+
+
+$message=""; //will hold message to display when authenticated
+
+//checks if username and password match what is in the database
+// if(count($_POST)>0) {
+	$conn = mysqli_connect($servername, $username, $password, $dbname);
+	$result = mysqli_query($conn,"SELECT * FROM `users` JOIN `songs` ON users.username = songs.username WHERE songs.username = 'ccatura';");
+
+    while ($row = mysqli_fetch_assoc($result)) {
+        echo "<li>".$row['song_title']." <br>".$row['artist']."<br/>".$row['song_lyrics'];
+    }
+?>
+
+<!-- 
+<?php
     $readJson = file_get_contents('./users/ccatura/songs/song_list.json');
     $songs = json_decode($readJson, true);
     // $sortAs = $_GET['sort'];
@@ -11,11 +34,8 @@
     {
        $sortAs = $_GET['sort'];
     } else {
-        $sortAs = 'artist';
+        $sortAs = 'setlist';
     }
-
-    
-    echo "<li>* Only the first song works, so far.\n";
 
     foreach($songs as $item) {
         $songsnew = array_column($item, $sortAs); // sort by setlist
@@ -26,4 +46,4 @@
         }
     }
 
-?>
+?> -->
