dcf180a9d9a4a43ed7815633ea2544f2fa410651
ccatura <charlie@charliecatura.com>
2022-03-17 14:41:51 -0400

    made progress on create page

---
+++ create.js
@@ -0,0 +1,53 @@
+var wrapper = document.querySelector('.wrapper');
+var customField = document.querySelector('.custom-field');
+
+wrapper.addEventListener('change', function() {
+    var thisSelection = event.target; // this is the selection dropdown list with the choices: verse, chorus, etc.
+    var parentId = thisSelection.parentElement.id; // this gets the ID of the parent element
+    var parent = document.querySelector('#' + parentId); // this gets the parent element of the selection dropdown list
+    var firstChild = parent.querySelector('.custom-field');
+
+    console.log(thisSelection);
+
+    if (thisSelection.value == 'custom') {
+        firstChild.style.display = "block";
+    } else {
+        firstChild.style.display = "none";
+    }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+// var wrapper = document.querySelector('.wrapper');
+// var customField = document.querySelector('.custom-field');
+
+// wrapper.addEventListener('change', function() {
+//     var wrapperClass = this.className;
+//     var thisSelection = event.target;
+//     var thisSelectionId = thisSelection.id;
+//     var thisSelectionPartName = thisSelection.value;
+//     var parentId = thisSelection.parentElement.id;
+//     var parent = document.querySelector('#' + parentId);
+//     var firstChild = parent.querySelector('.custom-field');
+    
+//     console.log("Parent Id: " + parentId);
+//     console.log("Child: " + firstChild);
+//     console.log("ID: " + thisSelectionId);
+//     console.log("Part Name: " + thisSelectionPartName);
+//     console.log("");
+
+//     if (thisSelectionPartName == 'custom') {
+//         firstChild.style.display = "block";
+//     } else {
+//         firstChild.style.display = "none";
+//     }
+// });
