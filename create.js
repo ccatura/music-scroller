1bdc52c3cda1610bc1be9e385c7c2ddc8286d353
ccatura <charlie@charliecatura.com>
2022-03-17 15:13:21 -0400

    condensed some code for create page

--- create.js
+++ create.js
@@ -3,18 +3,38 @@ var customField = document.querySelector('.custom-field');
 
 wrapper.addEventListener('change', function() {
     var thisSelection = event.target; // this is the selection dropdown list with the choices: verse, chorus, etc.
-    var parentId = thisSelection.parentElement.id; // this gets the ID of the parent element
-    var parent = document.querySelector('#' + parentId); // this gets the parent element of the selection dropdown list
-    var firstChild = parent.querySelector('.custom-field');
-
-    console.log(thisSelection);
+    var customField = event.target.parentElement.querySelector('.custom-field'); // this gets the input field with the class name custom-field
 
     if (thisSelection.value == 'custom') {
-        firstChild.style.display = "block";
+        customField.style.display = "block";
     } else {
-        firstChild.style.display = "none";
+        customField.style.display = "none";
     }
 });
+
+
+
+
+
+
+
+
+
+
+// wrapper.addEventListener('change', function() {
+//     var thisSelection = event.target; // this is the selection dropdown list with the choices: verse, chorus, etc.
+//     var parentId = thisSelection.parentElement.id; // this gets the ID of the parent element
+//     var parent = document.querySelector('#' + parentId); // this gets the parent element of the selection dropdown list
+//     var firstChild = parent.querySelector('.custom-field');
+
+//     console.log(thisSelection);
+
+//     if (thisSelection.value == 'custom') {
+//         firstChild.style.display = "block";
+//     } else {
+//         firstChild.style.display = "none";
+//     }
+// });
 
 
 
