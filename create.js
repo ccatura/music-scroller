520a37c7e2375d31750e305753976681b3a0f62e
ccatura <charlie@charliecatura.com>
2023-03-16 10:40:50 -0400

    re-coded some button actions to not be buggy and redundant. Working smoothly as of right now

--- create.js
+++ create.js
@@ -1,22 +1,13 @@
-var createContainer = document.querySelector('.create-container');
-var addBelow = document.querySelector('.add-below');
-var confirmRemoveBox = document.querySelector('.background-block');
-var confirmRemoveYes = document.querySelector('#confirm-remove-yes');
-var confirmRemoveNo  = document.querySelector('#confirm-remove-no');
-
-
+var createContainer     =  document.querySelector('.create-container');
+var addBelow            =  document.querySelector('.add-below');
+var confirmRemoveBox    =  document.querySelector('.background-block');
+var songSections        =  document.querySelector('.song-sections');
+var currentRemove;
+var previousColor;
 
 createContainer.addEventListener('click', function() {
     var thisSelection = event.target;
     
-    if(thisSelection.classList.contains('clicker')) {
-        var motherSection = getMotherSection(thisSelection, 'mother-section').className;
-        console.log('ID: ' + thisSelection.id + '\nClass List: ' + thisSelection.className);
-        console.log('Mother Section: ' + motherSection);
-        console.log('');
-    }
-
-
     // Adds section above or below current one - or removes section
     if (thisSelection.className.includes('actionbutton')) {
         var targetParentSection = getMotherSection((event.target), "song-part-mother-section"); 
@@ -30,17 +21,43 @@ createContainer.addEventListener('click', function() {
             clone.id = Math.random().toString().slice(2,20);
             clone.querySelector('.song-info-textarea').value = '';
         } else if (thisSelection.className.includes('remove')) {
-            var previousColor = window.getComputedStyle(targetParentSection).getPropertyValue('background-color');
-            confirmRemove(targetParentSection.id);
-            var currentRemove = targetParentSection.id;
+            if (songSections.childElementCount > 1) {
+                previousColor = window.getComputedStyle(targetParentSection).getPropertyValue('background-color');
+                confirmRemove(targetParentSection.id);
+                currentRemove = targetParentSection.id;
+            }
         } else if (thisSelection.className.includes('move-up')) {
             targetParentSection.parentNode.insertBefore(targetParentSection, targetParentSection.previousElementSibling);
         } else if (thisSelection.className.includes('move-down')) {
             targetParentSection.parentNode.insertBefore(targetParentSection.nextElementSibling, targetParentSection);
+        } 
+    } else if (thisSelection.className.includes('confirm-button')) {
+        currentRemove = confirmRemoveBox.getAttribute('name');
+        if (thisSelection.className.includes('confirm-remove-yes')) {
+            confirmRemoveBox.style.display = 'none';
+            document.getElementById(currentRemove).remove();
+        } else if (thisSelection.className.includes('confirm-remove-no')) {
+            confirmRemoveBox.style.display = 'none';
+            document.getElementById(currentRemove).style.backgroundColor = previousColor;
         }
-
-
     }
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -52,38 +69,11 @@ createContainer.addEventListener('click', function() {
         document.getElementById(id).style.backgroundColor = 'red';
     }
     
-    confirmRemoveYes.addEventListener('click', function() {
-        confirmRemoveBox.style.display = 'none';
-        try {
-            document.getElementById(currentRemove).remove();
-        } catch(err) {
-            console.log(err);
-        }
-    });
-    confirmRemoveNo.addEventListener('click', function() {
-        confirmRemoveBox.style.display = 'none';
-        try {
-            document.getElementById(currentRemove).style.backgroundColor = previousColor;
-        } catch(err) {
-            console.log(err);
-        }
-    });
-
-
-
-
-
     function getMotherSection(targetElement, classNameToFind) {
         const sect = classNameToFind;
-        var parentSection = targetElement;
-
-        try {
-            while (!parentSection.className.includes(sect)) {
-                parentSection = parentSection.parentElement;
-            }
-        } catch (err) {
-            console.log('ERROR: ' + err);
-            return targetElement;
+        var parentSection = targetElement.parentElement;
+        while (!parentSection.className.includes(sect)) {
+            parentSection = parentSection.parentElement;
         }
         return parentSection;
     }
